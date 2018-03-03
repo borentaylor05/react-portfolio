9d34d113ac23f113a3e7530d649d1d262f037805
Taylor Boren <tboren@homeaway.com>
2018-03-03 12:16:59 -0600

    fixed lambda

--- upload-portfolio.lamda.py
+++ upload-portfolio.lamda.py
@@ -4,16 +4,43 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:123322973314:PortfolioBuildComplete')
 
-portfolio_bucket = s3.Bucket('portfolio.taylorboren.me')
-build_bucket = s3.Bucket('portfoliobuild.taylorboren.me')
+    location = {
+        'bucketName': 'portfoliobuild.taylorboren.me',
+        'objectKey': 'portfoliobuild.zip'
+    }
+    
+    try:
+        job = event.get('CodePipeline.job')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'MyAppBuild':
+                    location = artifact['location']['s3Location']
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print 'Building portfolio from ' + str(location)
+
+        portfolio_bucket = s3.Bucket('portfolio.taylorboren.me')
+        build_bucket = s3.Bucket('portfoliobuild.taylorboren.me')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+        topic.publish(Subject="Portfolio Deployed!", Message="Deploy successful")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed", Message="Deploy NOT successful")
+        raise
+    
